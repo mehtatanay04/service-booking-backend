b384efef985d948c364c57f6744c03711ba2ac22
Tanay Mehta <mehtatanay40@gmail.com>
2026-01-30 23:36:40 +0530

    Added User entity and UserRepository with JPA configuration

---
+++ src/main/java/com/tanay/bookingapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.tanay.bookingapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tanay.bookingapp.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+User findByEmail(String email);
+
+}
