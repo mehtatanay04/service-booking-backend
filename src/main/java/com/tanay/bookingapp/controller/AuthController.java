3213ef2276f1bf610385ba9601443343bb6b67d6
Tanay Mehta <mehtatanay40@gmail.com>
2026-02-01 00:58:54 +0530

    Implemented user registration API with Spring Boot and JPA

---
+++ src/main/java/com/tanay/bookingapp/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.tanay.bookingapp.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tanay.bookingapp.entity.User;
+import com.tanay.bookingapp.service.UserService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	private final UserService userService;
+	public AuthController (UserService userService) {
+		this.userService = userService;
+		}
+	
+	@PostMapping("/register")
+	public User register(@RequestBody User user) {
+		return userService.registerUser(user);
+	}
+	
+}
